502156143176cc9a247c9c870488783e7e50b947
Your Name <rsmith5901@gmail.com>
2018-05-19 12:58:05 -0400

    decouple selector from item adder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { NgWeightInputComponent } from './ng-weight-input/ng-weight-input.component';
+import { Observable } from 'rxjs/Observable';
+import { Item } from '../models/item';
+import { map } from 'rxjs/operators';
+import { NgWeightSelectionComponent } from './ng-weight-selection/ng-weight-selection.component';
 
 enum WeightCostFormKeys {
   weight = 'weight',
@@ -8,22 +12,24 @@ enum WeightCostFormKeys {
   name = 'name'
 }
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  providers: [ NgWeightInputComponent ]
+  providers: [NgWeightInputComponent]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild(NgWeightInputComponent) weightInput: NgWeightInputComponent;
+  @ViewChild(NgWeightSelectionComponent) selectedItem: NgWeightSelectionComponent;
+
+  items$: Observable<Item[]>;
+  selectedItem$: Observable<Item>;
+
   weightForm: FormGroup;
 
   disableWeightCost = false;
 
-  constructor(
-    private readonly _formBuilder: FormBuilder,
-    private readonly _ngWeightInput: NgWeightInputComponent
-  ) {
+  constructor(private readonly _formBuilder: FormBuilder, private readonly _ngWeightInput: NgWeightInputComponent) {
     this.createWeightForm();
   }
 
@@ -39,4 +45,11 @@ export class AppComponent {
     return this.weightForm.get('itemsForm') as FormGroup;
   }
 
+  ngOnInit() {
+    const itemsCollection$ = this.weightInput.selectedItems as Observable<any>;
+    this.items$ = itemsCollection$.pipe(map(itemsCollection => itemsCollection.items));
+
+    const selectedItemCollection$ = this.selectedItem.selectedItemChange as Observable<any>;
+    this.selectedItem$ = selectedItemCollection$.pipe(map(itemsCollection => itemsCollection.selectedItem));
+  }
 }
